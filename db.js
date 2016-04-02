017b3debfffe97e83ed3512c38a3f834eaa6237e
joefche <75189343@qq.com>
2016-04-02 18:35:07 +0800

    guestbook

---
+++ db.js
@@ -0,0 +1,17 @@
+/*
+ author: joefche
+ date  : 2016-04-01
+ site  : nodejs.macaubus.com
+ */
+
+var knex = require('knex')({
+    client: 'mysql',
+    //debug: true,  //显示查询的 sql 语句
+    connection: {
+        host     : '127.0.0.1',
+        user     : 'root',
+        password : '123456',
+        database : 'node'
+    }
+});
+module.exports=knex;
---
+++ routes/async.js
@@ -0,0 +1,42 @@
+/*
+ author: joefche
+ date  : 2016-04-01
+ site  : nodejs.macaubus.com
+ */
+
+var knex = require('../db');
+var async = require('async');
+
+async.parallel({
+    total:function (callback) {
+        knex('article')
+            .count('* as total')
+            .then(function (data) {
+                callback(null, data[0].total);
+            })
+            .catch(function (err) {
+                callback(err);
+            });
+    },
+    rows:function (callback) {
+        knex('article')
+            .orderBy('id', 'desc')
+            .limit(10)
+            .offset(1)
+            .then(function (models) {
+                callback(null, models);
+            })
+            .catch(function (err) {
+                callback(err);
+            });
+    }
+
+}, function (err, result) {
+    var total = 0;
+    for (var footers in result.rows) {
+        total += result.rows[footers].id;
+    }
+    result.footer = [{id:total}];
+    console.log(result);
+
+});
